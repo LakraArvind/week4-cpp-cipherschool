84aff69c7bf6da85f83ecae78b073259c2c2fe3a
LakraArvind <125327980+LakraArvind@users.noreply.github.com>
2023-07-17 23:40:03 +0530

    Create mode.cpp

---
+++ day1/mode.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class A{
+    int a;
+    protected:
+    int b;
+    public:
+    int c;
+
+};
+class B: public A{
+    public:
+
+}
+int main(){
+    //A obj;
+    B obj;
+   // cout << obj.a<<obj.b<<obj.c<< endl;
+    return 0;
+}
